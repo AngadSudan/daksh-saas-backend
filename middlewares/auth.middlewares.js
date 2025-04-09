0f175b7248a16bf5e46c4d97c6f274a011b0bd1a
Angad Sudan <angadsudan453@gmail.com>
2025-04-10 00:37:34 +0530

    updated middleware testing

--- middlewares/auth.middlewares.js
+++ middlewares/auth.middlewares.js
@@ -1,10 +1,11 @@
 import ApiResponse from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 const verfiyJWT = async (req, res, next) => {
-  console.log("cookies are ", req.cookies);
+  // console.log("cookies are ", req.headers);
   // console.log("headers are ", req.headers);
   const token =
     req.cookies.token || req.header("Authorization")?.replace("Bearer ", "");
+  // console.log(token);
   if (!token) {
     return res
       .status(200)
@@ -15,21 +16,7 @@ const verfiyJWT = async (req, res, next) => {
   if (!decodedToken)
     return res.status(200).json(new ApiResponse(500, "invalid token", null));
 
-  // const user = await prism.user.findUnique({
-  //   where: { id: decodedToken.id },
-  //   select: { id: true, name: true, email: true },
-  // });
-  // console.log(user);
-  // if (!user)
-  //   return res.status(200).json(new ApiResponse(500, "No user found", null));
   req.user = decodedToken;
-  // req.user = {
-  //   id: token,
-  // };
-  // req.user = {
-  //   id: "f3799626-ba67-4479-9c3f-0f00729a47b6",
-  //   ,   "6cc68bc7-ebbe-4669-962b-dcd654462abb" ||
-  // };
 
   next();
 };
