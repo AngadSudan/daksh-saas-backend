9cf6a43de296188cf563b8f45c3fea2c9197adee
Angad Sudan <angadsudan453@gmail.com>
2025-04-14 01:46:10 +0530

    added changes for the deployment

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ app.use("/api", limiter);
 app.use(hpp());
 
 //express middlewares
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 app.use(cookieParser());
 app.use(
   cors({
